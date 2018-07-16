7e7f25e85f12e6be59ac06b2438326e368f71189
vincent hais <haisvincentapb@hotmail.com>
2018-07-16 18:15:19 +0200

    Class History and activity_history.xml added

---
+++ app/src/main/java/com/example/megaport/moodtracker/Controllers/History.java
@@ -0,0 +1,15 @@
+package com.example.megaport.moodtracker.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.megaport.moodtracker.R;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
